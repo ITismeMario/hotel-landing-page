caf35c3dfa9e6fd219c4bab3d01edf160fe66382
Mario A. Rodríguez <marm218@outlook.com>
2022-01-07 20:09:29 -0600

    feat: added the user model for MongoDB

---
+++ backend/models/userModel.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const UserSchema = new mongoose.Schema(
+	{
+		firstName: {
+			type: String,
+			require: true,
+			min: 2,
+			max: 20,
+		},
+		lastName: {
+			type: String,
+			require: true,
+			min: 2,
+			max: 20,
+		},
+		email: {
+			type: String,
+			required: true,
+			max: 50,
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: true,
+			min: 6,
+		},
+		role: {
+			type: String,
+			enum: ['user', 'admin'],
+			default: 'user',
+		},
+	},
+	{ timestamps: true }
+);
+
+UserSchema.pre('save', async function (next) {
+	if (!this.isModified('password')) return next();
+
+	this.password = await bcrypt.hash(this.password, 10);
+	next();
+});
+
+UserSchema.methods.isValidPassword = async function (enteredPassword) {
+	return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model('user', UserSchema);
+module.exports = User;
