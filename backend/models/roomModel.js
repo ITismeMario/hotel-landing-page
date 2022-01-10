9c6a1d8df72e5ab63ada3f6286a90dee76cbf851
Mario A. Rodríguez <marm218@outlook.com>
2022-01-10 09:40:45 -0600

    feat: Added the room schema

---
+++ backend/models/roomModel.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const roomDetailsSchema = new mongoose.Schema({
+	room_details_icon: { type: String, required: true },
+	room_details_description: { type: String, required: true },
+});
+
+const roomAmenitiesSchema = new mongoose.Schema({
+	room_amenities_icon: { type: String, required: true },
+	room_amenities_description: { type: String, required: true },
+});
+
+const roomSchema = new mongoose.Schema(
+	{
+		name: { type: String, required: true, unique: true },
+		images: { type: Array, required: true },
+		description: { type: String, required: true },
+		price: { type: Number },
+		details: [roomDetailsSchema],
+		services: [roomAmenitiesSchema],
+	},
+	{ timestamps: true }
+);
+
+const Room = mongoose.model('room', roomSchema);
+module.exports = Room;
