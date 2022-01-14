84f3954cac9b4bf62bf768a263e6d9cebb4b3eda
Mario A. Rodríguez <marm218@outlook.com>
2022-01-13 20:49:06 -0600

    feat: Added the update Room functionality
    The validations and unit test for this feature were added too

--- backend/test/roomTest.js
+++ backend/test/roomTest.js
@@ -8,6 +8,8 @@ chai.use(chaiHttp);
 
 //Stores the docs ID to delete them all a te end by iterating throug this array
 const createdDocumentID = [];
+const goodToken =
+	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZTA1YjRhYWJiNmMzM2ZlYWJjOTc1YiIsImlhdCI6MTY0MjA5MzM5MCwiZXhwIjoxNjQ0Njg1MzkwfQ.PD5s36nRxCb-tCtPT9WoMWR27afKypBCllqm1H3TnmA';
 
 const createTestData = {
 	goodRoom: {
@@ -161,6 +163,51 @@ const createTestData = {
 	],
 };
 
+const UpdateTestData = {
+	goodRoom: {
+		name: faker.random.words(),
+		images: [
+			'https://res.cloudinary.com/dypchsfgk/image/upload/v1641767091/Hotel/rooms/studioroom_luywjo.jpg',
+			'https://res.cloudinary.com/dypchsfgk/image/upload/v1641767091/Hotel/rooms/studioroom_luywjo.jpg',
+		],
+		description: faker.random.words(),
+		price: faker.datatype.number(),
+		details: [
+			{
+				room_details_icon: 'https://res.cloudinary.com/dypchsfgk/image/upload/v1641768186/Hotel/details/details-person-dark_rhv9l3.svg',
+				room_details_description: faker.random.words(),
+			},
+		],
+		amenities: [
+			{
+				room_amenities_icon: 'https://res.cloudinary.com/dypchsfgk/image/upload/v1641763616/Hotel/amenities_icons/wifi_qz8mip.svg',
+				room_amenities_description: faker.random.words(),
+			},
+		],
+	},
+	badNameRoom: {
+		name: 'Unit Testing Room', //already exists
+		images: [
+			'https://res.cloudinary.com/dypchsfgk/image/upload/v1641767091/Hotel/rooms/studioroom_luywjo.jpg',
+			'https://res.cloudinary.com/dypchsfgk/image/upload/v1641767091/Hotel/rooms/studioroom_luywjo.jpg',
+		],
+		description: faker.random.words(),
+		price: faker.datatype.number(),
+		details: [
+			{
+				room_details_icon: 'https://res.cloudinary.com/dypchsfgk/image/upload/v1641768186/Hotel/details/details-person-dark_rhv9l3.svg',
+				room_details_description: faker.random.words(),
+			},
+		],
+		amenities: [
+			{
+				room_amenities_icon: 'https://res.cloudinary.com/dypchsfgk/image/upload/v1641763616/Hotel/amenities_icons/wifi_qz8mip.svg',
+				room_amenities_description: faker.random.words(),
+			},
+		],
+	},
+};
+
 describe('*Rooms*', () => {
 	// CREATE ROOM
 	it('it should POST a room', (done) => {
@@ -211,6 +258,52 @@ describe('*Rooms*', () => {
 				res.should.have.status(201);
 				res.body.should.be.a('object');
 				res.body.should.have.property('_id').eql(id);
+				done();
+			});
+	});
+
+	// UPDATE ROOM
+	it('it should PUT a room', (done) => {
+		const id = createdDocumentID.slice(-1).pop();
+		chai.request(server)
+			.put(`/api/rooms/${id}`)
+			.set('Authorization', `Bearer ${goodToken}`)
+			.send(UpdateTestData.goodRoom)
+			.end((err, res) => {
+				res.should.have.status(201);
+				res.body.should.be.an('object');
+				res.body.should.include.keys('_id');
+				createdDocumentID.push(res.body._id);
+				done();
+			});
+	});
+
+	createTestData.incompleteRooms.map((room) => {
+		it('it should NOT PUT a room with missing data', (done) => {
+			const id = createdDocumentID.slice(-1).pop();
+			chai.request(server)
+				.put(`/api/rooms/${id}`)
+				.set('Authorization', `Bearer ${goodToken}`)
+				.send(room)
+				.end((err, res) => {
+					res.should.have.status(403);
+					res.body.should.be.an('object');
+					res.body.should.have.property('errors');
+					done();
+				});
+		});
+	});
+
+	it('it should NOT PUT a room with a name already asigned to another room', (done) => {
+		const id = createdDocumentID.slice(-1).pop();
+		chai.request(server)
+			.put(`/api/rooms/${id}`)
+			.set('Authorization', `Bearer ${goodToken}`)
+			.send(UpdateTestData.badNameRoom)
+			.end((err, res) => {
+				res.should.have.status(403);
+				res.body.should.be.an('object');
+				res.body.should.have.property('errors');
 				done();
 			});
 	});
